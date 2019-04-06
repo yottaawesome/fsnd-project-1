369f8c53950ef312461c6c163436e3790c86952d
Vasilios Magriplis <etameson@gmail.com>
2019-04-06 21:55:12 +0930

    * Initial commit

---
+++ src/sql/most-errors-by-day.sql
@@ -0,0 +1,29 @@
+select 
+	errors.date, 
+	errors.total_errors, 
+	totals.total_requests, 
+	round((cast(errors.total_errors as decimal)/totals.total_requests) * 100, 5) as error_rate
+from 
+(
+	select 
+		date_trunc('day', time) as date, 
+		count(*) as total_errors
+	from 
+		log
+	where 
+		status <> '200 OK'
+	group by date
+	order by date
+) as errors
+inner join 
+(
+	select 
+		date_trunc('day', time) as date, 
+		count(*) as total_requests
+	from 
+		log
+	group by date
+	order by date
+) as totals on errors.date = totals.date
+where 
+	round((cast(errors.total_errors as decimal)/totals.total_requests) * 100, 5) > 1;
