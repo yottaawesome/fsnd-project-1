62e323450e39b7c3a2d2c32d9548d83dc57980c4
Vasilios Magriplis <etameson@gmail.com>
2019-04-08 21:21:57 +0930

    * Minor fixes ** Fix pycodestyle issues ** Clean up shebang

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-#!./env/bin/python3
+#!/usr/bin/env python3
 '''The main module.'''
 
 from newsdb import top_articles, top_authors, top_error_days
@@ -10,8 +10,8 @@ def print_options():
     print('Enter one of the following options:')
     print('1: view most popular articles')
     print('2: view the most popular authors')
-    print('3: view the days where failed requests amounted to more than 1% ' \
-        'of requests on that day')
+    print('3: view the days where failed requests amounted to more than 1% '
+          'of requests on that day')
     print('r: review options again')
     print('q: quit')
 
@@ -44,5 +44,6 @@ def main():
 
         else:
             print('Invalid input {}'.format(selection))
+
 
 main()
